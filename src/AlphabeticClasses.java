fd6b6ff7d1fa03db9abc713fc8b301c1b83c5b6b
Cole DeBoer <Cole@Thes-MacBook-Pro-2.local>
2019-05-10 16:39:52 -0500

    sup

---
+++ src/AlphabeticClasses.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Arrays.*;
+public class AlphabeticClasses {
+	public static void main(String[]args) {
+		
+		
+String myClasses[] = {"Honors English", "Honors Physical Science", "Robotics Engineering Mechanisms", "Honors Geometry", "Spanish 2", "Honors Social Studies", "Explorations in Coding"};
+	Arrays.sort(myClasses);
+	System.out.println("Your classes ordered alphabetically are: ");
+	for(int i = 0; i < myClasses.length; i++) { 
+		System.out.println(myClasses[i]);
+		
+		
+		
+		
+	}
+		
+		
+		
+		
+		
+	}
+	
+
+}
